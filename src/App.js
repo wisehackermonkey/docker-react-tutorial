ef36c0f65c96a2c4a1d7b9b45e2dfbff13cfdaa6
wisehackermonkey <oranbusiness@gmail.com>
2019-03-08 12:52:33 -0800

    removed password from travis setup

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           Welcome to the <code>World</code>
+           Hello, welcome to my world
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            by learning docker cli
+            by wisemonkey
           </a>
         </header>
       </div>
